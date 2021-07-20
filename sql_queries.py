3b40a055b2b52ddfc5fb5324e37866ddf2db88d1
yugyesh <uges.kk@gmail.com>
2021-07-20 16:40:03 +0900

    feat: Add sql queries to drop tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,6 +1,14 @@
+# DROP TABLE QUERIES
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+users_table_drop = "DROP TABLE IF EXISTS users"
+songs_table_drop = "DROP TABLE IF EXISTS songs"
+artists_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+
 # CREATE TABLE QUERIES
-songplay_table_create = """CREATE TABLE IF NOT EXISTS songplay
-(songplay_id int, start_time int, user_id int, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar)"""
+songplay_table_create = """CREATE TABLE IF NOT EXISTS songplay(songplay_id int, start_time int, user_id int,
+ level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar)"""
 
 users_table_create = """CREATE TABLE IF NOT EXISTS users
 (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar)"""
@@ -20,4 +28,12 @@ create_table_queries = [
     songs_table_create,
     artists_table_create,
     time_table_create,
+]
+
+drop_table_queries = [
+    songplay_table_drop,
+    users_table_drop,
+    songs_table_drop,
+    artists_table_drop,
+    time_table_drop,
 ]
