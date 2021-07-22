ba4e251e387d75f97272a5d55d9e4e916d85ece2
yugyesh <uges.kk@gmail.com>
2021-07-22 16:21:30 +0900

    fix: Add on conflict condition on sql_queries

--- sql_queries.py
+++ sql_queries.py
@@ -29,13 +29,13 @@ song_table_insert = """INSERT INTO songs(song_id, title, artist_id, year, durati
 VALUES(%s, %s, %s, %s, %s)"""
 
 artist_table_insert = """INSERT INTO artists(artist_id, name, location, latitude, longitude)
-VALUES(%s, %s, %s, %s, %s)"""
+VALUES(%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING"""
 
 time_table_insert = """INSERT INTO time(start_time, hour, day, week, month, year, weekday)
 VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING"""
 
 user_table_insert = """INSERT INTO users(user_id, first_name, last_name, gender, level)
-VALUES(%s, %s, %s, %s, %s)"""
+VALUES(%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING"""
 
 songplay_table_insert = """INSERT INTO songplays(start_time, user_id, level, song_id, artist_id,
 session_id, location, user_agent)
